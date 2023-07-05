4b1b09d87a214ada641765264683a820e03355b8
eimaam <imamddahir@gmail.com>
2023-07-05 17:09:07 +0100

    updated packages

--- src/interfaces.ts
+++
@@ -1,6 +0,0 @@
-interface Details {
-    message: string;
-    backgroundColor?: string,
-    textColor?: string,
-    position?: "right" | "top-right" | "bottom-right" | "left" | "top-left" | "bottom-left"
-  }
