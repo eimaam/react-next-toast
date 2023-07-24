f3185200214ca1274df5f0ea4e2710939742d35e
eimaam <imamddahir@gmail.com>
2023-07-24 14:06:18 +0100

    chore: update info icon to tally with the use

--- src/partials/InfoIcon.tsx
+++ src/partials/InfoIcon.tsx
@@ -3,43 +3,43 @@ import React from "react";
 const InfoIcon = () => {
   return (
     <svg
-      width="26px"
-      height="26px"
-      viewBox="0 0 24 24"
-      fill="none"
-      xmlns="http://www.w3.org/2000/svg"
-    >
-      <g id="Iconly/Curved/warning Square">
-        <g id="warning Circle">
-          <path
-            id="Stroke 1"
-            fill-rule="evenodd"
-            clip-rule="evenodd"
-            d="M2.74976 12C2.74976 5.063 5.06276 2.75 11.9998 2.75C18.9368 2.75 21.2498 5.063 21.2498 12C21.2498 18.937 18.9368 21.25 11.9998 21.25C5.06276 21.25 2.74976 18.937 2.74976 12Z"
-            stroke="#130F26"
-            stroke-width="1.5"
-            stroke-linecap="round"
-            stroke-linejoin="round"
-          />
-          <path
-            id="Stroke 3"
-            d="M11.9998 8.10498V12"
-            stroke="#130F26"
-            stroke-width="1.5"
-            stroke-linecap="round"
-            stroke-linejoin="round"
-          />
-          <path
-            id="Stroke 15"
-            d="M11.9955 15.5H12.0045"
-            stroke="#130F26"
-            stroke-width="1.5"
-            stroke-linecap="round"
-            stroke-linejoin="round"
-          />
-        </g>
-      </g>
-    </svg>
+  width="26px"
+  height="26px"
+  viewBox="0 0 24 24"
+  fill="none"
+  xmlns="http://www.w3.org/2000/svg"
+>
+  <g id="Iconly/Curved/Info Square">
+    <g id="Info Square">
+      <path
+        id="Stroke 1"
+        fill-rule="evenodd"
+        clip-rule="evenodd"
+        d="M21.2502 12C21.2502 18.937 18.9372 21.25 12.0002 21.25C5.06324 21.25 2.75024 18.937 2.75024 12C2.75024 5.063 5.06324 2.75 12.0002 2.75C18.9372 2.75 21.2502 5.063 21.2502 12Z"
+        stroke="#130F26"
+        stroke-width="1.5"
+        stroke-linecap="round"
+        stroke-linejoin="round"
+      />
+      <path
+        id="Stroke 3"
+        d="M12.0002 15.895V12"
+        stroke="#130F26"
+        stroke-width="1.5"
+        stroke-linecap="round"
+        stroke-linejoin="round"
+      />
+      <path
+        id="Stroke 15"
+        d="M12.0045 8.5H11.9955"
+        stroke="#130F26"
+        stroke-width="1.5"
+        stroke-linecap="round"
+        stroke-linejoin="round"
+      />
+    </g>
+  </g>
+</svg>;
   );
 };
 
