efc0c507465acb4904c9406add4cfa80f579ec71
eimaam <imamddahir@gmail.com>
2023-07-24 10:44:31 +0100

    feat: added custom positioning, toast type, toast duration etc..

--- src/partials/interfaces.ts
+++ src/partials/interfaces.ts
@@ -1,7 +1,28 @@
 export interface ToastDetail {
-    type: "success" | "error" | "info" | "warning"  
-    message: string;
-    backgroundColor?: string,
-    textColor?: string,
-    position?: "right" | "top-right" | "bottom-right" | "left" | "top-left" | "bottom-left",
-  }
+  type: "success" | "error" | "info" | "warning";
+  message: string;
+  backgroundColor?: string;
+  textColor?: string;
+  position?:
+    | "right"
+    | "top-right"
+    | "bottom-right"
+    | "left"
+    | "top-left"
+    | "bottom-left";
+  duration?: number;
+}
+
+export enum TOAST_TYPE {
+  success = "success",
+  error = "error",
+  warning = "warning",
+  info = "info",
+}
+
+export enum DEFAULT_MESSAGE {
+  success = "Success",
+  error = "Error",
+  warning = "Warning",
+  info = "Info",
+}
