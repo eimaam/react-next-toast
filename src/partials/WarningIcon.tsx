f212bfe534bdfcccf6a671d9e64277650c008195
eimaam <imamddahir@gmail.com>
2023-07-24 10:47:58 +0100

    feat: setup various toast types icons

--- src/partials/WarningIcon.tsx
+++ src/partials/WarningIcon.tsx
@@ -3,8 +3,8 @@ import React from "react";
 const WarningIcon = () => {
   return (
     <svg
-      width="800px"
-      height="800px"
+      width="26px"
+      height="26px"
       viewBox="0 0 24 24"
       fill="none"
       xmlns="http://www.w3.org/2000/svg"
